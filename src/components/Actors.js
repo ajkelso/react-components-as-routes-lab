32a3a0a7ecbc3f008ab2f35f0a537c6d2538df42
ajkelso <ajkelso@gmail.com>
2021-03-08 13:14:43 -0500

    commit tree 8b578277067384875c5fd19e9a2eead7ffb0f705

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,9 +2,23 @@ import React from 'react';
 import { actors } from '../data';
 
 const Actors = () => {
+  const renderActors = () => {
+    return actors.map(actor => {
+      const renderMovies = () => {
+        return actor.movies.map(movie => <li>{movie}</li>)
+      }
+      return <div>
+        <h2>{actor.name}</h2>
+        <strong>Movies</strong>
+        <ul>{renderMovies()}</ul>
+      </div>
+    })
+  }
+  
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {renderActors()}
     </div>
   );
 };
